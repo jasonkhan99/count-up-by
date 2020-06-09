f2303f545ad2305d21b55793f1be30ed301b0391
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-09 10:46:27 -0700

    add if else with a nested for loop
    Co-authored-by:  Joseph Pearce <joseph.pearce@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
-    
+    const countTo = parseInt($("#input1").val());
+    const countBy = parseInt($("#input2").val());
+
+    if (isNaN(countTo)) {
+      alert("please enter a numerical value")
+    } else if (countTo < 0 || countBy < 0) {
+      alert("please enter a number greater than 0")
+    } else {
+      for (let index = 0; index <= countTo; index += countBy) {
+      console.log(index)
+      }
+    }
   });
 });
+
+
+// function milliseconds(x) {
+//   if (isNaN(x)) {
+//     return 'Not a Number!';
+//   }
+//   return x * 1000;
+// }
+
+// console.log(milliseconds('100F'));
+// // expected output: "Not a Number!"
+
+// console.log(milliseconds('0.0314E+2'));
+// // expected output: 3140
